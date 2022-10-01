ccc48011902381a6c84ea511df1600e8bf84bfdc
Kali <kalilinuxuser5@gmail.com>
2022-10-01 22:44:49 +0530

    Feat:Initial Commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 
 if __name__=="__main__":
-    os.system("cd venv\Scripts && activate")
-    os.system("pip install -r requirements.txt")
-    os.system("python database.py")
-    os.system("python main.py")
+    os.system("cd venv\Scripts && activate") # navigate to the venv folder
+    os.system("pip install -r requirements.txt") # install all the required modules
+    os.system("python database.py") # Create Datbase and table
+    os.system("python main.py") # Open Up the localhost server 
